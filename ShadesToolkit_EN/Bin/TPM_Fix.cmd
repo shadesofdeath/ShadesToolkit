7056032e484b492cb5ebd583e5aa6c10bf2760d0
Berkay AY <61064047+shadesofdeath@users.noreply.github.com>
2023-04-22 02:03:28 +0300

    :+1:

--- ShadesToolkit_EN/Bin/TPM_Fix.cmd
+++
@@ -1,5 +0,0 @@
-reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassTPMCheck" /t REG_DWORD /d 1
-reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassRAMCheck" /t REG_DWORD /d 1
-reg add "HKLM\SYSTEM\Setup\LabConfig" /v "BypassSecureBootCheck" /t REG_DWORD /d 1
-echo reg patch ran OK > X:\TPMFIX.TXT
-exit
